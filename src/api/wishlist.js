1bc3197d43889f316ceb2f1d2946a370dbc5d6bb
chitranshjawre <chitranshjawre2001@gmail.com>
2025-09-17 10:22:38 +0530

    update

--- src/api/wishlist.js
+++ src/api/wishlist.js
@@ -1,7 +1,7 @@
 import httpClient from "../utils/httpClient";
 import endpoints from "../utils/endpoint";
 export const wishlistApi = {
-  get: () => httpClient.get("/wishlist"),
+  get: () => httpClient.get("/api/wishlist"),
   add: (productId) => httpClient.post("/wishlist/add", { productId }),
   remove: (productId) => httpClient.post("/wishlist/remove", { productId }),
 };
